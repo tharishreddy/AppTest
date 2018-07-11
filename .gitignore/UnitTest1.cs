99fc4cd55376af19d317bc6562c983681f93bb6f
Harish <tharishreddy@gmail.com>
2018-07-11 13:46:18 +0100

    Add files via upload

---
+++ .gitignore/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿namespace AppTests
+{
+	using System.Threading.Tasks;
+	using ApprovalTests;
+	using ApprovalTests.Reporters;
+	using NUnit.Framework;
+
+	[TestFixture]
+	public class UnitTest1
+	{
+		[Test]
+		[UseReporter(typeof(DiffReporter), typeof(NUnitReporter))]
+		public async Task Abc()
+		{
+			await Task.Delay(1);
+			Approvals.Verify("abc");
+		}
+	}
+}
